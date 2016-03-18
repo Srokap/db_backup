bb8e7ba1f50a78e99a3ee5bd67b24143ab6572b5
Facyla ~ Florian DANIEL <facyla@gmail.com>
2016-03-18 15:43:17 +0100

    Added French language file

---
+++ languages/fr.php
@@ -0,0 +1,18 @@
+<?php
+$french = array(
+	'admin:backup' => "Backup",
+	'admin:backup:database' => "Backup de la base de données",
+	'db_backup:cli' => "Outils CLI",
+	'db_backup:cli:dependencies:ok' => "Tous les dépendances requises sont présentes.",
+	'db_backup:cli:dependencies:fail' => "Il manque des dépendances. Ce plugin peut ne pas fonctionner correctement !",
+	'db_backup:backup' => "Backup",
+	'db_backup:dir:validate:fail' => "Impossible de créer le répertoire de backup, ou l'utilisateur n'a pas les droits d'écriture dessus. Chemin : %s",
+	'db_backup:restore' => "Restaurer",
+	'db_backup:restore:backup_file' => "Choisissez un fichier de backup à partir duquel restaurer la base de données : ",
+	'db_backup:button:quick' => "Backup DB",
+	'db_backup:action:backup:ok' => "La base de données a bien été sauvegardée dans le fichier : %s",
+	'db_backup:action:backup:fail' => "Une erreur s'est produite lors de la création du backup. Erreur : %d: %s",
+	'db_backup:action:restore:ok' => "La base de données a bien été restaurée à partir du fichier : %s",
+	'db_backup:action:restore:fail' => "Une erreur s'est produite lors de la restauration de la base de données. Erreur : %d: %s",
+);
+add_translation('fr', $french);
